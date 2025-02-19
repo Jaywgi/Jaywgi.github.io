5620cdf2d66c3085892e5f2d516ec873de8a43c1
Jaywgi <JayJayThePlaya@outlook.com>
2025-02-19 06:56:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('nav ul li a');
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-            window.scrollTo({
-                top: targetSection.offsetTop,
-                behavior: 'smooth'
-            });
-        });
-    });
+    console.log('JavaScript is loaded and working!');
 });
